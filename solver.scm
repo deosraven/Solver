dfd7d496fe31f9bfc31d810985f0aefb2cf66856
Adrien <deosraven@hotmail.com>
2019-04-23 19:40:34 +0200

    fonctionnel

--- solver.scm
+++ solver.scm
@@ -1,43 +1,86 @@
 #lang racket
 
+(require data/heap)
+
 ;un node du graphe de recherce composé d'un état, de son node parent et de la direction prise pour l'atteinde, null pour l'état initial.
 (struct node (state parent symbol))
 
 (provide rp-solve)
-;(provide rp-solve-heuristic)
+(provide rp-solve-heuristic)
 
 ; Si `s` est l'état initial d'un puzzle régulier, `adj` est sa fonction
 ; d'adjacence, `acc-state?` est le predicat accepteur de ce puzzle,
 ; (rp-solve s adj acc-state?) est un itérateur paresseux de solutions
 ; du puzzle. Les mots du langage sont donnés par ordre de longueur. 
-(define rp-solve
-  (lambda (s adj acc-state?)
-    (rp-solve-acc (list (node s #f #f)) adj acc-state?)))
+(define (rp-solve s adj acc-state?)
+  (rp-solve-acc (list (node s #f #f)) adj acc-state?))
 
-(define rp-solve-acc
-  (lambda (queue adj acc-state?)
-    (let ((current (car queue)))
-      (cond
-        [(null? queue) (lambda () '())]
-        [(acc-state? (node-state current))
-         (lambda () (cons (build-solution current)
-                          (rp-solve-acc (cdr queue) adj acc-state?)))]
-        [else (rp-solve-acc (append (cdr queue)
-                                   (map (lambda (pair) (node (cdr pair) current (car pair)))
-                                        (filter-map (lambda (pair) (and (not (is-cyclic-state current (cdr pair))) pair))
-                                                    (adj (node-state current)))))
-                           adj
-                           acc-state?)]))))
+(define (rp-solve-acc queue adj acc-state?)
+  (if (null? queue)
+      (lambda () '())
+      (let ((current (car queue)))
+        (if (acc-state? (node-state current))
+            (lambda () (cons (build-solution current)
+                             (rp-solve-acc (cdr queue) adj acc-state?)))
+            (rp-solve-acc (append (cdr queue)
+                                  (map (lambda (pair) (node (cdr pair) current (car pair)))
+                                       (filter-map (lambda (pair) (and (not (is-cyclic-state current (cdr pair))) pair))
+                                                   (adj (node-state current)))))
+                          adj
+                          acc-state?)))))
 
-(define is-cyclic-state
-  (lambda (parent state)
-    (cond [(not parent) #f]
-          [(equal? (node-state parent) state) #t]
-          [else (is-cyclic-state (node-parent parent) state)])))
+(define (is-cyclic-state parent state)
+  (cond [(not parent) #f]
+        [(equal? (node-state parent) state) #t]
+        [else (is-cyclic-state (node-parent parent) state)]))
 
-(define build-solution
-  (lambda (node)
-    (if (node-parent node)
-        (append (build-solution (node-parent node))
-                (list (node-symbol node)))
-        '())))
+(define (build-solution node)
+  (if (not (node-parent node)) '()
+      (append (build-solution (node-parent node))
+              (list (node-symbol node)))))
+
+
+; Si `s` est l'état initial d'un puzzle régulier, `adj` est sa fonction
+; d'adjacence, `acc-state?` est le predicat accepteur de ce puzzle et
+; `heuristic` est une fonction d'heuristic pour ce puzzle,
+; (rp-solve s adj acc-state? heuristic) est un itérateur paresseux de solutions
+; du puzzle. Les mots du langage sont donnés dans l'ordre de l'heuristique
+(define (rp-solve-heuristic s adj acc-state? heuristic)
+  (let ((queue (make-priority)))
+    (priority-insert! queue (node s #f #f) (heuristic s))
+    (rp-solve-heuristic-acc queue adj acc-state? heuristic)))
+
+(define (rp-solve-heuristic-acc queue adj acc-state? heuristic)
+  (if (priority-empty? queue)
+      (lambda () '())
+      (let ((current (priority-pop! queue)))
+        (if (acc-state? (node-state current))
+            (lambda () (cons (build-solution current)
+                             (rp-solve-heuristic-acc queue adj acc-state? heuristic)))
+            (begin
+              (map (lambda (pair)
+                     (priority-insert! queue
+                                       (node (cdr pair) current (car pair))
+                                       (heuristic (cdr pair))))
+                   (filter-map (lambda (pair) (and (not (is-cyclic-state current (cdr pair))) pair))
+                               (adj (node-state current))))
+              (rp-solve-heuristic-acc queue
+                                      adj
+                                      acc-state?
+                                      heuristic))))))
+
+
+;; Priority queue based on heap
+(define (make-priority)
+  (make-heap (lambda (x y) (<= (second x) (second y)))))
+
+(define (priority-insert! queue x priority) 
+  (heap-add! queue (list x priority)))
+ 
+(define (priority-pop! queue)
+  (begin0 
+    (first (heap-min queue))
+    (heap-remove-min! queue)))
+
+(define (priority-empty? queue)
+  (= 0 (heap-count queue)))
--- taquin.scm
+++ taquin.scm
@@ -3,78 +3,116 @@
 (provide taquin-make-state)
 (provide taquin-adj-states)
 (provide taquin-acc-state?)
-;(provide taquin-heuristic)
+(provide taquin-heuristic)
 
-; Etats du taquin comprenant un vecteur représentant le board et l'index de la case vide dans ce vecteur 
-(struct state (board blank) :#transparent)
+; Etats du taquin comprenant un vecteur représentant le board, l'index de la case vide dans ce vecteur et la taille d'un côté du taquin (N pour taquin N*N)
+(struct state (board blank size) #:transparent)
 
 ; La fonction taquin-make-state prend en entrée une liste de N
 ; listes. La i-ème sous-liste représente la i-ème ligne du taquin, en
 ; commençant à compter depuis le haut. Les nombres sont numérotés à partir de 1
 ; et le trou est symbolisé par le symbole 'x.
 ; La fonction renvoie la représentation de l'état correspondant.
-; Peut probablement être optimisé pour passer de O(2n) à O(n) en faisant flatten + find-blank en une opération
-; Input: lls: Une liste de N listes de taille N représentant un taquin comprenant chaque nombre de 1 à (N*N)-1 une fois et le charactère x représentant la case vide
-; Output: L'état correspondant à la liste
-(define taquin-make-state
-  (lambda (lls)
-    (let ((flat (flatten lls)))
-      (state (list->vector flat)
-                    (taquin-find-blank flat 0)))))
+(define (taquin-make-state lls)
+  (let ((flat (flatten lls)))
+    (state (list->vector flat)
+           (taquin-find-blank flat 0)
+           (sqrt (length flat)))))
 
-; La Fonction taquin-find-blank permet de trouver la case vide (représentée par un x) dans une liste représentant un board.
-; Input: list: Une liste représentant un board de taquin et contenant donc un x représentant la case vide
-;        position: Un nombre représentant l'index de début de recherche dans le tableau
-; Output: L'index de la case vide dans la liste
-; Elle n'est assurée que si elle est appellée avec position valant 0.
-(define taquin-find-blank
-  (lambda (list position)
-    (if (or (null? list)
-            (eq? (car list) 'x))
-        position
-        (taquin-find-blank (cdr list) (+ 1 position)))))        
-
-;(define (flatten x)
-;  (cond ((null? x) '())
-;        ((pair? x) (append (flatten (car x))
-;                           (flatten (cdr x))))
-;        (else (list x))))
+; Si `list` est une liste de taille N*N représentant un jeu de taquin (et contenant donc les nombres de 1 à (N*N)-1 et un 'x représentant une case vide)
+; et `position` vaut 0 alors (taquin-find-blank list position) renvoie l'index de la case vide dans list.
+(define (taquin-find-blank list position)
+  (if (or (null? list)
+          (eq? (car list) 'x))
+      position
+      (taquin-find-blank (cdr list) (+ 1 position))))         
     
-; Si `state` est la représentation d'un état, (taquin-adj-states state)
+; Si `st` est la représentation d'un état, (taquin-adj-states st)
 ; renvoie une liste de paire pointées, dont le car est le symbole menant
 ; à l'état représenté par le cdr.
-(define taquin-adj-states
-  (lambda (st)
-    (append (move-left st)
-            (move-up st)
-            (move-right st)
-            (move-down st))))
+(define (taquin-adj-states st)
+  (append (move-left st)
+          (move-up st)
+          (move-right st)
+          (move-down st)))
 
-; (taquin-acc-state? state) renvoie vrai si et seulement si `state` est la
-; représentation de l'état accepteur
-(define taquin-acc-state?
-  (lambda (state)
-    (let ((board (state-board state)))
-      (equal? board (list->vector (taquin-solution 1 (vector-length board)))))))
+; (taquin-acc-state? st) renvoie vrai si et seulement si `st` est la
+; représentation de l'état accepteur.
+(define (taquin-acc-state? state)
+  (let ((board (state-board state)))
+    (equal? board (list->vector (taquin-solution 1 (vector-length board))))))
 
+; Si `size` est le nombre de case d'un jeu de taquin (= N*N) et `current` vaut 0 alors (taquin-solution current size) renvoie une liste de taille N*N comprenant dans l'ordre les nombres 1 à (N*N)-1
+; puis x et correspondant au jeu de taquin résolu.
 (define taquin-solution
   (lambda (current size)
     (if (= current size)
         '(x)
         (cons current (taquin-solution (+ 1 current) size)))))
 
-; Si `state` est la représentation d'un état, (taquin-heuristic state)
-; renvoie...
-; H1 - nombre de cases mal placées
-;(define taquin-heuristic
-; (lambda (state)
-;   'TODO'))
+; Si `st` est la représentation d'un état, (taquin-heuristic st)
+; renvoie le nombre d'éléménts mal placés par rapport à la solution
+(define taquin-heuristic
+  (lambda (st)
+    (count-different (vector->list (state-board st))
+                     (taquin-solution 1 (vector-length (state-board st))))))
+
+; Si `left` et `right` sont deux listes de tailles identiques alors (count different left right) renvoie le nombre de différences entre les deux listes.
+(define count-different
+  (lambda (left right)
+    (if (or (null? left) (null? right))
+        0
+        (+ (if (equal? (car left) (car right)) 0 1) (count-different (cdr left) (cdr right))))))
 
 ; H2 - distance de manhattan
 ; (define taquin-heuristic
 ;   (lambda (state)
 ;     'TODO))
 
+; Si `state` est la représentation d'un état, (taquin-heuristic state)
+; renvoie le nombre d'éléménts mal placés par rapport à la solution
+(define (taquin-heuristic-wrong-place state)
+  (count-different (vector->list (state-board state))
+                   (taquin-solution 1 (vector-length (state-board state)))))
+
+(define (count-different left right)
+  (if (or (null? left) (null? right))
+      0
+      (+ (if (equal? (car left) (car right)) 0 1) (count-different (cdr left) (cdr right)))))
+
+(define (taquin-heuristic state)
+  (taquin-heuristic-list (state-size state)
+                         0
+                         (vector->list (state-board state))))
+
+(define (taquin-heuristic-list size index list)
+  (if (or (null? list) (equal? (car list) 'x)) 0
+      (+ (taquin-heuristic-list size (+ 1 index) (cdr list))
+         ; right is the coordinates of the index where the current value should be
+         ; current is the coordinates of the current index
+         (let ((right (index-to-coordinates size (- (car list) 1)))
+               (current (index-to-coordinates size index)))
+           (+ (abs (- (car right)
+                      (car current)))
+              (abs (- (cdr right)
+                      (cdr current))))))))
+
+(define (index-to-coordinates size index)
+  (cons (floor (/ index size)) (modulo index size)))  
+
+(define (move st direction condition pos)
+  (if condition
+      (list (cons direction
+                  (state (vector-copy-swap (state-board st)
+                                           (state-blank st)
+                                           (+ (state-blank st) pos))
+                         (+ (state-blank st) pos)
+                         (state-size st))))
+      null))
+
+; Si `st` est un état, `direction` est une des directions de déplacement possible, `condition` est une condition correspondant aux restriction de mouvement
+; d'une case vide en fonction de `direction` et `pos` est le modificateur de position permettant en l'ajoutant à la position de la case vide de la déplacer dans `direction` 
+; alors (move st direction condition pos) renvoie une paire pointée dont le car est la direction et le cdr est l'état obtenu en déplacant la case vide dans cette direction.
 (define move
   (lambda (st direction condition pos)
     (if condition
@@ -82,44 +120,47 @@
                     (state (vector-copy-swap (state-board st)
                                              (state-blank st)
                                              (+ (state-blank st) pos))
-                           (+ (state-blank st) pos))))
-        null)))    
+                           (+ (state-blank st) pos)
+                           (state-size st))))
+        null)))
 
+; Si `st` est un état alors (move-left st) renvoie une paire pointée dont le car est 'l et le cdr est le nouvel état obtenu en déplacant la case vide vers la gauche.  La fonction renvoie null si c'est impossible.
 (define move-left
   (lambda (st)
     (move st
           'l
-          (not (equal? (modulo (state-blank st)
-                               (sqrt (vector-length (state-board st))))
-                       0))
+          (not (equal? (modulo (state-blank st) (state-size st)) 0))
           -1)))
 
+; Si `st` est un état alors (move-up st) renvoie une paire pointée dont le car est 'u et le cdr est le nouvel état obtenu en déplacant la case vide vers le haut.  La fonction renvoie null si c'est impossible.
 (define move-up
   (lambda (st)
     (move st
           'u
-          (>= (state-blank st)
-              (sqrt (vector-length (state-board st))))
-          (- (sqrt (vector-length (state-board st)))))))
+          (>= (state-blank st) (state-size st))
+          (- (state-size st)))))
 
+; Si `st` est un état alors (move-right st) renvoie une paire pointée dont le car est 'r et le cdr est le nouvel état obtenu en déplacant la case vide vers la droite.  La fonction renvoie null si c'est impossible.
 (define move-right
   (lambda (st)
     (move st
           'r
           (not (equal? (modulo (state-blank st)
-                               (sqrt (vector-length (state-board st))))
-                       (- (sqrt (vector-length (state-board st))) 1)))
+                               (state-size st))
+                       (- (state-size st) 1)))
           1)))
 
+; Si `st` est un état alors (move-down st) renvoie une paire pointée dont le car est 'd et le cdr est le nouvel état obtenu en déplacant la case vide vers le bas.  La fonction renvoie null si c'est impossible.
 (define move-down
   (lambda (st)
     (move st
           'd
           (< (state-blank st)
              (- (vector-length (state-board st))
-                (sqrt (vector-length (state-board st)))))
-          (sqrt (vector-length (state-board st))))))
+                (state-size st)))
+          (state-size st))))
 
+; Si `v` est un vecteur, `pos1` est un un index dans ce vecteur et `pos2` est un index dans ce vecteur, alors (vector-copy-swap v pos1 pos2) renvoie 
 (define vector-copy-swap
   (lambda (v pos1 pos2)
     (let ((copy (vector-copy v)))
